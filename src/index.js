a6797ef178e7a2e28515fb4335c69bbf15df0abf
Anna Gombay <apgombay@edu.uwaterloo.ca>
2020-02-18 11:17:51 -0500

    FINAL COMMIT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.modulecss';
+import './index.module.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
