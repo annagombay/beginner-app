d6d8e9e547f46f96e868d6fa2c52b8079013aebf
Anna Gombay <apgombay@edu.uwaterloo.ca>
2020-03-25 00:50:24 -0400

    axio

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
-import classes from './ContactData.module.css'
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 
-
 class ContactData extends Component {
-    state= {
+    state = {
         name: '',
         email: '',
         address: {
@@ -15,18 +15,19 @@ class ContactData extends Component {
         },
         loading: false
     }
-    orderHandler = (event) => {
+
+    orderHandler = ( event ) => {
         event.preventDefault();
         this.setState( { loading: true } );
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
             customer: {
-                name: 'Anna Gombay',
+                name: 'Max Schwarzmüller',
                 address: {
-                    street: 'Dalton Rd',
-                    city: 'Toronto',
-                    country: 'Canada'
+                    street: 'Teststreet 1',
+                    zipCode: '41351',
+                    country: 'Germany'
                 },
                 email: 'test@test.com'
             },
@@ -34,29 +35,34 @@ class ContactData extends Component {
         }
         axios.post( '/orders.json', order )
             .then( response => {
-                this.setState( { loading: false, purchasing: false } );
+                this.setState( { loading: false } );
+                this.props.history.push('/');
             } )
             .catch( error => {
-                this.setState( { loading: false, purchasing: false } );
+                this.setState( { loading: false } );
             } );
-        console.log(this.props.ingredients);
     }
 
-render () {
-    return (
-        <div className={classes.ContactData}>
-            <h4>Enter your Contact Data</h4>
+    render () {
+        let form = (
             <form>
-                <input className={classes.Input}type="text" name="name" placeholder="Your name" />
-                <input className={classes.Input}type="email" name="email" placeholder="Your email" />
-                <input className={classes.Input}type="text" name="street" placeholder="Your street" />
-                <input className={classes.Input}type="text" name="postal" placeholder="Your postal code" />
+                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-              
             </form>
-        </div>
-    )
-}
+        );
+        if ( this.state.loading ) {
+            form = <Spinner />;
+        }
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter your Contact Data</h4>
+                {form}
+            </div>
+        );
+    }
 }
 
-export default ContactData; 
+export default ContactData;
